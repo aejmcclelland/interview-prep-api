ea36e5870775b11b2b76110e2c316772e8a6d858
aejmcclelland <aejmcclelland@googlemail.com>
2026-01-15 09:08:47 +0000

    Bootstrap Spring Boot app with Postgres(Docker)

---
+++ src/main/java/uk/co/amcclelland/interview_prep_api/InterviewPrepApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.amcclelland.interview_prep_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/amcclelland/interview_prep_api/InterviewPrepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.amcclelland.interview_prep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
